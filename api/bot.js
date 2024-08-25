7934d5f317ad0d4d0f0af50e05587d61098c92e6
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2024-08-25 09:01:19 +0200

    refactor: bot now has some test commands and better formatting of the available listings from the GET request to the scrapper

--- api/bot.js
+++ api/bot.js
@@ -5,8 +5,30 @@ import express from 'express';
 const COMMANDS = {
     START: 'start',
     HELP: 'help',
+    TEMP: 'temp',
     GET_LISTINGS: 'getlistings'
 };
+
+
+const getFormattedListingData = (listings) => {
+        try {
+          if (!listings || listings.length === 0) {
+            return "No listings available at the moment.";
+          }
+      
+          let formattedMessage = "Here are the latest listings:\n\n";
+          listings.forEach(listing => {
+            const { url, rent, availableFrom, size } = listing;
+            const baseUrl = "https://www.wg-gesucht.de"; 
+            formattedMessage += `[${size} for €${rent}, available from ${availableFrom}](${baseUrl}${url})\n\n`;
+          });
+          return formattedMessage;
+        } catch (error) {
+          console.error("Error formatting listing data:", error);
+          return "An error occurred while fetching the listings.";
+        }
+};
+
 
 if(!process.env.BOT_TOKEN || !process.env.SCRAPPER_SERVER_URL) {
     throw new Error(`Please provide BOT_TOKEN and SCRAPPER_SERVER_URL in the .env file`);
@@ -14,6 +36,8 @@ if(!process.env.BOT_TOKEN || !process.env.SCRAPPER_SERVER_URL) {
 
 const bot = new Bot(process.env.BOT_TOKEN);
 const GET_LISTINGS_URL = process.env.SCRAPPER_SERVER_URL;
+
+console.log('bot.js \t 16 GET_LISTINGS_URL: ', GET_LISTINGS_URL);
 
 bot.command(COMMANDS.START, (ctx) => {
  console.log('Received /start command');
@@ -25,6 +49,11 @@ bot.command(COMMANDS.HELP, (ctx) => {
  ctx.reply('Send /start to start the bot.');
 });
 
+bot.command(COMMANDS.TEMP, (ctx) => {
+    console.log('Received /temp command');
+    ctx.reply('This is a temporary command.');
+});
+
 // Respond to the /getlistings command
 bot.command(COMMANDS.GET_LISTINGS, async (ctx) => {
     try {
@@ -32,11 +61,13 @@ bot.command(COMMANDS.GET_LISTINGS, async (ctx) => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        // console.log('bot.js \t 33 response: ', JSON.parse(response.json()));
         const data = await response.json();
         console.log('bot.js \t 34 data: ', data);
-        ctx.reply(`Data fetched: ${data}`);
+        ctx.reply(`Data fetched: ${getFormattedListingData(data)}`);
     } catch (error) {
-        console.error(error);
+        console.error('error ----', error);
         ctx.reply(`Failed to fetch data: ${error.message}`);
     }
 });
